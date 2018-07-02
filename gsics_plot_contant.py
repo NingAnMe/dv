8f067fb19ddec95c1eb2afba67d3b03a4f2b07a2
NingAnMe <ninganme@qq.com>
2018-07-02 16:18:25 +0800

    add

---
+++ gsics_plot_contant.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2018/6/29 15:39
+@Author  : AnNing
+"""
+ORG_NAME = 'NSMC-GPRC'
+
+EDGE_GRAY = '#303030'
+
+RED = '#f63240'
+BLUE = '#1c56fb'
+GRAY = '#c0c0c0'
+
+if __name__ == "__main__":
+    pass
